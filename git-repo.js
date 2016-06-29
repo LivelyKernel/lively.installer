860800402d41a73873b2fb3b7e1afd58de03e203
Robert Krahn <robert.krahn@gmail.com>
2016-06-28 20:59:36 -0700

    git repo: stageOrUnstageOrDiscardFiles, add(files), fileStatus, commit, init

--- git-repo.js
+++ git-repo.js
@@ -10,6 +10,15 @@ async function test() {
 
 }
 
+function parentdir(dir) {
+  return dir.split("/").slice(0,-1).join("/");
+}
+
+function dirname(dir) {
+  var pathParts = dir.split("/");
+  return pathParts[pathParts.length-1];
+}
+
 export class Repository {
 
   constructor(directory, options = {dryRun: false, log: []}) {
@@ -148,8 +157,160 @@ export class Repository {
   }
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  // commit
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  commit(message, all = false) { return this.cmd(`git commit -${all ? "a" : ""}m '${message}'`); }
+
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   // local commit state
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  async fileStatus(options) {
+    /*
+    var results = await new Repository("/Users/robert/Lively/LivelyKernel2").fileStatus()
+    */
+
+    options = options || {};
+
+    var self = this,
+        fileObjects = await fileStatus(this.directory);
+    fileObjects = fileObjects.map(addFilenameAndChange);
+    return fileObjects;
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    async function fileStatus(dir, next) {
+      var {code, output} = await self.cmd("git status --porcelain");
+      if (code) throw new Error("git status failed: " + output);
+      var lines = output.split('\n');
+      var results = lines && lines.reduce(function(lines, line) {
+        if (!line) return lines;
+        var m, results = lines;
+        if (m = line.match(/^(\s[A-Z]|[A-Z]{2})(.*)/)) results.push({status: "unstaged",  statusString: m[0]});
+        if (m = line.match(/^[A-Z]{1,2}(.*)/))         results.push({status: "staged",    statusString: m[0]});
+        if (m = line.match(/^\s*\?\?(.*)/))            results.push({status: "untracked", statusString: m[0]});
+        return results;
+      }, []).filter(ea => !!ea);
+      return results
+    }
+
+    function addFilenameAndChange(fileObject) {
+        // statusString looks like "R  bar.txt -> foo.txt"
+        //        +o   ' ' = unmodified
+        //        +o    _M = modified
+        //        +o    _A = added
+        //        +o    _D = deleted
+        //        +o    _R = renamed
+        //        +o    _C = copied
+        //        +o    _U = updated but unmerged
+        //
+        //        Ignored files are not listed, unless --ignored option is in effect, in
+        //        which case XY are !!.
+        //
+        //            X          Y     Meaning
+        //            -------------------------------------------------
+        //                      [MD]   not updated
+        //            M        [ MD]   updated in index
+        //            A        [ MD]   added to index
+        //            D         [ M]   deleted from index
+        //            R        [ MD]   renamed in index
+        //            C        [ MD]   copied in index
+        //            [MARC]           index and work tree matches
+        //            [ MARC]     M    work tree changed since index
+        //            [ MARC]     D    deleted in work tree
+        //            -------------------------------------------------
+        //            D           D    unmerged, both deleted
+        //            A           U    unmerged, added by us
+        //            U           D    unmerged, deleted by them
+        //            U           A    unmerged, added by them
+        //            D           U    unmerged, deleted by us
+        //            A           A    unmerged, both added
+        //            U           U    unmerged, both modified
+        //            -------------------------------------------------
+        //            ?           ?    untracked
+        //            !           !    ignored
+        //            -------------------------------------------------
+
+        var statusString = fileObject.statusString,
+            type= fileObject.status,
+            change = '',
+            fileName = statusString.slice(3),
+            statusFlags = statusString.slice(0,2), // git status --porcelain format
+            statusFlagIndex = statusFlags[0],
+            statusFlagWorkTree = statusFlags[1],
+            statusFlag = type === 'unstaged' ? statusFlagWorkTree : statusFlagIndex;
+
+        // for unmerged changes the status flags can be interpreted as follows:
+        if (statusFlags ==="DD") change = "unmerged, deleted locally and remotely";
+        else if (statusFlags ==="AU") change = "unmerged, added locally and modified remotely";
+        else if (statusFlags ==="UD") change = "unmerged, modified locally and deleted remotely";
+        else if (statusFlags ==="UA") change = "unmerged, modified locally and added remotely";
+        else if (statusFlags ==="DU") change = "unmerged, deleted locally and modified remotely";
+        else if (statusFlags ==="AA") change = "unmerged, added locally and remotely";
+        else if (statusFlags ==="UU") change = "unmerged, modified locally and remotely";
+        else if (statusFlag === "M") change = 'modfied';
+        else if (statusFlag === "R") change = 'renamed';
+        else if (statusFlag === "C") change = 'copied';
+        else if (statusFlag === "A") change = 'added';
+        else if (statusFlag === "D") change = 'deleted';
+
+        if (change === 'renamed' || change === 'copied')
+          fileName = fileName.split('->').last().trim();
+
+        fileObject.change = change;
+        fileObject.fileName = fileName;
+        return fileObject;
+    }
+
+  }
+
+  async add(files) {
+    // files = {fileName}
+    return this.cmd(`git add ${files.map(ea => typeof ea === "string" ? ea : ea.fileName).join(" ")}`);
+  }
+
+  async stageOrUnstageOrDiscardFiles(action, fileObjects, options) {
+    // action = "stage" || "unstage"
+    // fileObjects come from git.fileStatus and should have a fileName and status property
+    // patches should be lively.ide.FilePatch objects
+    // EITHER fileObjects or patches are needed. patches take precdence
+
+    console.assert(action === "stage" || action === "unstage" || action === "discard", action + " is not expected action");
+    console.assert(fileObjects, "stageOrUnstage needs file status objects");
+
+    options = options || {};
+
+    var filter;
+    if (action === "stage") filter = function(fo) { return fo.status === "unstaged"; };
+    if (action === "unstage") filter = function(fo) { return fo.status === "staged"; };
+    if (action === "discard") filter = function(fo) { return true; };
+
+    var cmdGroups = fileObjects.reduce(function(cmds, fo) {
+      if (!filter(fo))  return cmds;
+      var groups = [];
+      if (action === "unstage" || action === "discard") groups.push(cmds.reset);
+      if (action === "discard") groups.push(cmds.checkout);
+      if ((action === "unstage" || action === "discard") && fo.status === "staged" && fo.change === 'added') groups.push(cmds.rmCached);
+      if (action === "stage" && fo.status === "unstaged" && fo.change === 'deleted') groups.push(cmds.rm);
+      else if (action === "stage") groups.push(cmds.add);
+      groups.invoke("push", fo.fileName);
+      return cmds;
+    }, {checkout: [], reset: [], rmCached: [], rm: [], add: []});
+
+    var commands = [];
+    if (cmdGroups.rm.length)       commands.push({name: "rm",       gitCommand: "git rm -- "          + cmdGroups.rm.join(" ")});
+    if (cmdGroups.rmCached.length) commands.push({name: "rm",       gitCommand: "git rm --cached -- " + cmdGroups.rmCached.join(" ")});
+    if (cmdGroups.add.length)      commands.push({name: "add",      gitCommand: "git add -- "         + cmdGroups.add.join(" ")});
+    if (cmdGroups.reset.length)    commands.push({name: "reset",    gitCommand: "git reset -- "       + cmdGroups.reset.join(" ")});
+    if (cmdGroups.checkout.length) commands.push({name: "checkout", gitCommand: "git checkout -- "    + cmdGroups.checkout.join(" ")});
+
+    for (let cmd of commands) {
+      var {code, output} = await this.cmd(cmd.gitCommand);
+      if (code) throw new Error(`Error when trying to do git ${cmd.name}: ${output}`);
+    }
+
+  }
 
   stash() { return this.cmd("git stash"); }
   stashPop() { return this.cmd("git stash pop"); }
@@ -248,18 +409,29 @@ export class Repository {
 
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  // clone
+  // repo creation
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
   async clone(repoURL, branch = "master") {
-    var pathParts = this.directory.split("/"),
-        parentDir = pathParts.slice(0,-1).join("/"),
-        name = pathParts[pathParts.length-1];
     if ((await this.cmd(`test -d "${this.directory}"`)).code === 0) {
       throw new Error(`Cannot clone into ${this.directory}: exists already`);
     }
-    var {code, output} = await this.cmd(`git clone -b ${branch} ${repoURL} ${name}`, {cwd: parentDir});
-    if (code) throw new Error(`Failure cloning repo: ${output}`);
+    var cmd = await this.cmd(`git clone -b ${branch} ${repoURL} ${dirname(this.directory)}`, {cwd: parentdir(this.directory)});
+    if (cmd.code) throw new Error(`Failure cloning repo: ${cmd.output}`);
+    return cmd;
+  }
+
+  async init(optRepoURL) {
+    var {code, output} = await this.cmd(`mkdir -p ${this.directory}`, {cwd: parentdir(this.directory)});
+    if (code) {
+      throw new Error(`Could not initialize new git repo, creating a directory failed: ${this.directory}, ${output}`)
+    }
+    var cmd = await this.cmd(`git init`);
+    if (cmd.code) throw new Error(`Failure cloning repo: ${cmd.output}`);
+    if (optRepoURL) {
+      await this.cmd(`git remote add origin "${optRepoURL}"`);
+    }
+    return cmd;
   }
 
 }
